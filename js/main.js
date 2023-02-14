efe32a05ef94c009bdade0f771373f84d8950357
mohamedzrn <mohammedzrn13@gmail.com>
2023-02-14 18:45:20 -0500

    added content to the desktop version

---
+++ js/main.js
@@ -0,0 +1,120 @@
+var productsContainer = document.getElementById("productsContainer");
+      if (productsContainer) {
+        productsContainer.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var brandStoryContainer = document.getElementById("brandStoryContainer");
+      if (brandStoryContainer) {
+        brandStoryContainer.addEventListener("click", function (e) {
+          // Please sync "Quatro-Desktop-Brand Story" to the project
+        });
+      }
+      
+      var contactUsContainer = document.getElementById("contactUsContainer");
+      if (contactUsContainer) {
+        contactUsContainer.addEventListener("click", function (e) {
+          // Please sync "Quatro-Desktop-Contact Us" to the project
+        });
+      }
+      
+      var searchIcon = document.getElementById("searchIcon");
+      if (searchIcon) {
+        searchIcon.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var instagramIcon = document.getElementById("instagramIcon");
+      if (instagramIcon) {
+        instagramIcon.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var facebookIcon = document.getElementById("facebookIcon");
+      if (facebookIcon) {
+        facebookIcon.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var twitterIcon = document.getElementById("twitterIcon");
+      if (twitterIcon) {
+        twitterIcon.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var btnMoreContainer1 = document.getElementById("btnMoreContainer1");
+      if (btnMoreContainer1) {
+        btnMoreContainer1.addEventListener("click", function (e) {
+          // Please sync "Desktop- Product Page" to the project
+        });
+      }
+      
+      var btnMoreContainer3 = document.getElementById("btnMoreContainer3");
+      if (btnMoreContainer3) {
+        btnMoreContainer3.addEventListener("click", function (e) {
+          // Please sync "Desktop- Product Page" to the project
+        });
+      }
+      
+      var btnMoreContainer5 = document.getElementById("btnMoreContainer5");
+      if (btnMoreContainer5) {
+        btnMoreContainer5.addEventListener("click", function (e) {
+          // Please sync "Desktop- Product Page" to the project
+        });
+      }
+      
+      var btnMoreBrandStoryContainer = document.getElementById(
+        "btnMoreBrandStoryContainer"
+      );
+      if (btnMoreBrandStoryContainer) {
+        btnMoreBrandStoryContainer.addEventListener("click", function (e) {
+          // Please sync "Quatro-Desktop-Brand Story" to the project
+        });
+      }
+      
+      var productsContainer2 = document.getElementById("productsContainer2");
+      if (productsContainer2) {
+        productsContainer2.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var brandStoryContainer2 = document.getElementById("brandStoryContainer2");
+      if (brandStoryContainer2) {
+        brandStoryContainer2.addEventListener("click", function (e) {
+          // Please sync "Quatro-Desktop-Brand Story" to the project
+        });
+      }
+      
+      var contactUsContainer2 = document.getElementById("contactUsContainer2");
+      if (contactUsContainer2) {
+        contactUsContainer2.addEventListener("click", function (e) {
+          // Please sync "Quatro-Desktop-Contact Us" to the project
+        });
+      }
+      
+      var instagramIcon1 = document.getElementById("instagramIcon1");
+      if (instagramIcon1) {
+        instagramIcon1.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var facebookIcon1 = document.getElementById("facebookIcon1");
+      if (facebookIcon1) {
+        facebookIcon1.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
+      
+      var twitterIcon1 = document.getElementById("twitterIcon1");
+      if (twitterIcon1) {
+        twitterIcon1.addEventListener("click", function (e) {
+          // Please sync "Desktop-Default Page" to the project
+        });
+      }
